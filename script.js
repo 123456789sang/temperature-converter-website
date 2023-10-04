14622e62507f80c4f6fa3049dec206be2f134736
Sangram   Yadav <121220799+123456789sang@users.noreply.github.com>
2023-10-04 20:58:32 +0530

    Add files by upload

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
-console.log('Its working')
+let celsiusInput = document.querySelector('#celsius > input')
+let fahrenheitInput = document.querySelector('#fahrenheit > input')
+let kelvinInput = document.querySelector('#kelvin > input')
 
-let theme = localStorage.getItem('theme')
+let btn = document.querySelector('.button button')
 
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
+
+function roundNumber(number){
+    return Math.round(number*100)/100
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+
+/* Celcius to Fahrenheit and Kelvin */
+celsiusInput.addEventListener('input', function(){
+    let cTemp = parseFloat(celsiusInput.value)
+    let fTemp = (cTemp*(9/5)) + 32
+    let kTemp = cTemp + 273.15
+
+    fahrenheitInput.value = roundNumber(fTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
 
 
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
-}
+/* Fahrenheit to Celcius and Kelvin */
+fahrenheitInput.addEventListener('input', function(){
+    let fTemp = parseFloat(fahrenheitInput.value)
+    let cTemp = (fTemp - 32) * (5/9)
+    let kTemp = (fTemp -32) * (5/9) + 273.15
 
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
+    celsiusInput.value = roundNumber(cTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
+/* Kelvin to Celcius and Fahrenheit */
+kelvinInput.addEventListener('input', function(){
+    let kTemp = parseFloat(kelvinInput.value)
+    let cTemp = kTemp - 273.15
+    let fTemp = (kTemp - 273.15) * (9/5) + 32
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
+    celsiusInput.value = roundNumber(cTemp)
+    fahrenheitInput.value = roundNumber(fTemp)
+})
 
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
 
-	localStorage.setItem('theme', mode)
-}
+btn.addEventListener('click', ()=>{
+    celsiusInput.value = ""
+    fahrenheitInput.value = ""
+    kelvinInput.value = ""
+})
+
+	
